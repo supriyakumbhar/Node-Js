af97e33da128cad42edee90f6bcbb574404d0131
supriyakumbhar <95609232+supriyakumbhar@users.noreply.github.com>
2021-12-30 14:38:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,9 +6,33 @@ const express = require('express')
 //console.log(path.join(__dirname, '../public'))
 
 const app = express()
-
 const pathDirectory = path.join(__dirname, '/public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(pathDirectory))
+
+app.get('', (req,res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Supriya Kumbhar'
+  })
+})
+
+app.get('/about', (req, res) => {
+
+  res.render('about', {
+    title: 'About me',
+    name: 'Supriya kumbhar'
+
+  })
+})
+
+app.get('/help', (req,res) => {
+  res.render('help' , {
+    helpText: 'this is useful information.'
+  })
+
+})
 
 //app.get('' ,(req, res)  => {
   //  res.send('<h1>Weather</h1>')
